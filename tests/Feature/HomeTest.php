23bae06c34c5831f4cb86355191b0995768334d9
Laura Guo <lauraguo@proton.me>
2024-11-15 22:05:38 -0500

    Create feature tests for homepage

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\User;
+
+class HomeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void 
+    {
+        parent::setUp();
+
+        $this->seed();
+    }
+
+    public function test_home_for_guests_returns_a_successful_response(): void 
+    {
+        $response = $this->get(route('home'));
+        
+        $response->assertStatus(200);
+    }
+
+    public function test_home_for_users_returns_a_successful_response(): void 
+    {
+        $user = User::factory()->create();
+    
+        $response = $this->actingAs($user)->get(route('home'));
+        
+        $response->assertStatus(200);
+    }
+}
